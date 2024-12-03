845aa42f7c7cd3196d306173676ed18de7b0f688
main <alf318@naver.com>
2024-12-03 15:00:30 +0900

    up

--- Java/Hello/src/App.java
+++ Java/Hello/src/App.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! I'm Java!");
+        int n =14; //입력받은 정수
+        int count = 0;
+    //      반복문 생성
+    //      앞쪽 숫자
+        for(int i = 1; i <= n ; i++) {
+    //뒤쪽 숫자
+            for(int j = 1; j <= n ; j++) {
+                if (n % (i*j) == 0){
+                    count++;
+        System.out.println(n + "의 약수" + i +","+ j);
+                }
+            }
+    //          아래는 오케이
+             System.out.println("약수의 개수" + count + "개");
+        }
     }
 }
---
+++ Java/Hello/src/test.java
@@ -0,0 +1,18 @@
+public class test {
+    int n =14; //입력받은 정수
+    int count = 0;
+//      반복문 생성
+//      앞쪽 숫자
+    for(int i = 1; i <= n ; i++) {
+//뒤쪽 숫자
+        for(int j = 1; j <= n ; j++) {
+            if (n % (i*j) == 0){
+                count++;
+    System.out.println(n + "의 약수" + i +","+ j);
+            }
+        }
+//          아래는 오케이
+         System.out.println("약수의 개수" + count + "개");
+    }
+
+}
